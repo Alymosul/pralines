29f3201e3add71e88c59c74b48a223ade886cb4b
Aly Suleiman <alymosul@gmail.com>
2019-04-11 18:26:10 +0200

    Remove unnecessary import..

--- app/Http/Controllers/CoolProjectsController.php
+++ app/Http/Controllers/CoolProjectsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Actions\ActionBus;
 use App\Actions\AddProjectAction;
-use App\Validators\AddProjectValidator;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
